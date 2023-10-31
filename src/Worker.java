3ada36e6324cff5316c3defd6907a28941023fc4
Andrey <143511905+Iziumich@users.noreply.github.com>
2023-10-31 11:38:55 +0300

    Add files via upload

---
+++ src/Worker.java
@@ -0,0 +1,15 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("О нет! 33 ");
+            } else callback.onDone("Task " + i + " is done");
+        }
+    }
+}
